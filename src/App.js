9e5770b3644413df6c3e27482dcdfa822d658d4f
bellola <luisbg9@gmail.com>
2020-10-27 19:06:01 -0400

    added qb component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import QuestionBox from './components/QuestionsBox'
 
 
 class Welcome extends React.Component {
+  //initialize state with questions and score
  state = {
     'questionBank':[],
-  
+    'score': 0,
+    'responses': 0
   }
   //component method tha calls fixedQs and sets questions to state
   getQuestions = () => {
@@ -22,19 +24,41 @@ class Welcome extends React.Component {
   componentDidMount(){
     this.getQuestions()
   }
+  //check if answer matches the correct answer
+  computeAnswer(answer, correctAnswer){
+    if(answer === correctAnswer){
+      this.setState({
+        score:this.state.score + 1
+      })
+    }
+    this.setState({
+      responses: this.state.responses + 1
+    })
+    console.log('responses', this.state.responses)
+  }
+
+
   render() {
     console.log(this.state)
     return(
+    
       <div>
         {
         //when only rendering contidionally to make sure we only render questions
         //when they are in state
         this.state.questionBank  && 
-        this.state.questionBank.map(({question, correct, incorrect})=> 
+        this.state.responses < 10 &&
+        this.state.questionBank.map(({question, correct, incorrect, id})=> 
 
-        (<QuestionBox question={question} options={incorrect}/>)
+        (<QuestionBox 
+          question={question} 
+          options={incorrect} 
+          key={id}
+          selected={answer => this.computeAnswer(answer, correct)}/>)
+          
         
         )}
+        <h3>{this.state.responses}</h3>
         
       </div>
       
--- src/components/QuestionsBox.js
+++ src/components/QuestionsBox.js
@@ -1,13 +1,33 @@
 import React from 'react'
 
 
+class QuestionBox extends React.Component {
 
-const QuestionBox = ({question, options}) => {
-    return(
-        <div>
-            <div>{question}</div>
+    state = {
+        'answer':[]
+    }
+
+    componentDidMount(){
+        this.setState({answer:this.props.options})
+    }
+    render(){
+         return(
+        <div key={this.props.id}>
+            <div >{this.props.question}</div>
+            {   
+                this.state.answer.map((option) => (
+                <button 
+                onClick={()=>{
+                    this.setState({answer:[option]})
+                    this.props.selected(option)
+                    }}>
+                    {option}</button>))
+            }
+                
         </div>
     )
+    }
+   
 }
 
 export default QuestionBox
