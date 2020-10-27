090a16b97c0cab35b7b1cbcb5d43d70520ac5dae
bellola <luisbg9@gmail.com>
2020-10-27 13:35:34 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+
+class Welcome extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Hello</h1>;
+      </div>
+      
+    ) 
+  }
+}
+
+export default Welcome;
