c86cbcf67355388d387207adb3c1c9e49fda4a58
bellola <luisbg9@gmail.com>
2020-10-29 21:11:40 -0400

    working on readme

--- src/questions.js
+++ src/questions.js
@@ -1,40 +1,34 @@
-function Questions({questionBank, currentQuestion, handleAnswerOptionClick}){
-
-
-
-    return(
-      <>
-            <div className="question-section">
-              <div className="question-count">
-                <span>Question {currentQuestion + 1}</span>/
-                {questionBank.length}
-              </div>
-              <div className="question-text">
-                {questionBank[currentQuestion].question}
-              </div>
-            </div>
-            <div className="answer-section">
-              {questionBank[currentQuestion].incorrect
-                .sort(() => Math.random() - 0.5)
-                .map((answerOption) => (
-                  <button
-                    className={undefined}
-                    onClick={(e) =>
-                      handleAnswerOptionClick(
-                        e,
-                        answerOption,
-                        questionBank[currentQuestion]
-                          .correct
-                      )
-                    }
-                  >
-                    {answerOption}
-                  </button>
-                ))}
-            </div>
-          </>
-    )
+function Questions({ questionBank, currentQuestion, handleAnswerOptionClick }) {
+  return (
+    <>
+      <div className="question-section">
+        <div className="question-count">
+          <span>Question {currentQuestion + 1}</span>/{questionBank.length}
+        </div>
+        <div className="question-text">
+          {questionBank[currentQuestion].question}
+        </div>
+      </div>
+      <div className="answer-section">
+        {questionBank[currentQuestion].incorrect
+          .sort(() => Math.random() - 0.5)
+          .map((answerOption) => (
+            <button
+              className={undefined}
+              onClick={(e) =>
+                handleAnswerOptionClick(
+                  e,
+                  answerOption,
+                  questionBank[currentQuestion].correct
+                )
+              }
+            >
+              {answerOption}
+            </button>
+          ))}
+      </div>
+    </>
+  );
 }
 
-
-export default Questions
+export default Questions;
